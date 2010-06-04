ab0785495ed26cda4f18274b63598770da514f9f
vincentbluff <shaun@shaunfreeman.co.uk>
2010-06-04 22:32:30 +0100

    deleted unnecessary files and added server error page.

---
+++ Uthando-Error/error.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html
+    PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3c.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<link href="/images/favicon.ico" rel="shortcut icon" type="image/x-icon" />
+		<link href="/error/error.css" rel="stylesheet" type="text/css" />
+	</head>
+	<body>
+		<div id="uthandoLogo" class="center">
+			<img src="/images/404.png" />
+			<a href="http://<?php echo $_SERVER['SERVER_NAME'];?>">Click here to return to home page</a>
+		</div>
+	</body>
+</html>
