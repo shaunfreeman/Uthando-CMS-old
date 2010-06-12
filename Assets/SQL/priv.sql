f2351936a3d8ae3f968e9b86e46a19b5fccad521
vincentbluff <shaun@shaunfreeman.co.uk>
2010-06-12 10:31:16 +0100

    removed print_rr form menu class. oops!

--- Assets/SQL/priv.sql
+++ Assets/SQL/priv.sql
@@ -1,15 +1,15 @@
 #admin
-GRANT USAGE ON *.* TO 'uthando_admin'@'localhost' IDENTIFIED BY PASSWORD '*2470C0C06DEE42FD1618BB99005ADCA2EC9D1E19';
+GRANT USAGE ON *.* TO 'uthando_admin'@'localhost' IDENTIFIED BY 'password';
 GRANT SELECT, INSERT, UPDATE, DELETE ON `shaunfre\_core`.* TO 'uthando_admin'@'localhost';
 GRANT SELECT ON `shaunfre\_admin`.* TO 'uthando_admin'@'localhost';
-GRANT SELECT, INSERT, UPDATE, DELETE ON `UthandoCMS_sessions`.`shaunfre` TO 'uthando_admin'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON `UthandoCMS_sessions`.`shaun` TO 'uthando_admin'@'localhost';
 GRANT SELECT, INSERT, UPDATE, DELETE ON `uthando_ushop`.* TO 'uthando_admin'@'localhost';
 
 #user
-GRANT USAGE ON *.* TO 'uthando_user'@'localhost' IDENTIFIED BY PASSWORD '*2470C0C06DEE42FD1618BB99005ADCA2EC9D1E19';
+GRANT USAGE ON *.* TO 'uthando_user'@'localhost' IDENTIFIED BY 'password';
 GRANT SELECT ON `shaunfre\_core`.* TO 'uthando_user'@'localhost';
 GRANT INSERT (user_group_id, cdate, password, email_type_id, activation, username, email, first_name, iv, last_name, user_id, mdate), UPDATE (user_group_id, cdate, password, email_type_id, activation, username, email, first_name, iv, last_name, user_id, mdate) ON `shaunfre_core`.`users` TO 'uthando_user'@'localhost';
-GRANT SELECT, INSERT, UPDATE, DELETE ON `UthandoCMS_sessions`.`shaunfre` TO 'uthando_user'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON `UthandoCMS_sessions`.`shaun` TO 'uthando_user'@'localhost';
 GRANT SELECT ON `uthando\_ushop`.* TO 'uthando_user'@'localhost';
 GRANT INSERT, UPDATE, DELETE ON `uthando_ushop`.`order_items` TO 'uthando_user'@'localhost';
 GRANT INSERT, UPDATE, DELETE ON `uthando_ushop`.`shoppingcart` TO 'uthando_user'@'localhost';
@@ -18,9 +18,9 @@ GRANT INSERT, UPDATE, DELETE ON `uthando_ushop`.`orders` TO 'uthando_user'@'loca
 GRANT INSERT, UPDATE ON `uthando_ushop`.`user_cda` TO 'uthando_user'@'localhost';
 
 #guest
-GRANT USAGE ON *.* TO 'uthando_guest'@'localhost' IDENTIFIED BY PASSWORD '*2470C0C06DEE42FD1618BB99005ADCA2EC9D1E19';
+GRANT USAGE ON *.* TO 'uthando_guest'@'localhost' IDENTIFIED BY 'password';
 GRANT SELECT ON `shaunfre\_core`.* TO 'uthando_guest'@'localhost';
 GRANT INSERT (user_group_id, cdate, password, email_type_id, activation, username, email, first_name, iv, last_name, user_id, mdate) ON `shaunfre_core`.`users` TO 'uthando_guest'@'localhost';
-GRANT SELECT, INSERT, UPDATE, DELETE ON `UthandoCMS_sessions`.`shaunfre` TO 'uthando_guest'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON `UthandoCMS_sessions`.`shaun` TO 'uthando_guest'@'localhost';
 GRANT SELECT ON `uthando\_ushop`.* TO 'uthando_guest'@'localhost';
 GRANT INSERT, UPDATE, DELETE ON `uthando_ushop`.`shoppingcart` TO 'uthando_guest'@'localhost';
