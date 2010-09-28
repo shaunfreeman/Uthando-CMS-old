516348338d6a24d9a0243ae25a81f0e49fc61959
vincentbluff <shaun@shaunfreeman.co.uk>
2010-09-28 14:43:13 +0100

    content component changed to article.

--- Uthando-Lib/components/admin/article/action/new.php
+++ Uthando-Lib/components/admin/article/action/new.php
@@ -4,44 +4,53 @@
 defined( 'PARENT_FILE' ) or die( 'Restricted access' );
 
 if ($this->authorize()):
+
+	$sql = "
+		SELECT page_id, page, mdate
+		FROM ".$this->registry->core."pages
+	";
+	$result = $this->registry->db->query($sql);
 	
-	if (isset($this->registry->params['id'])):
+	$num_pages = count($result);
+	
+	if ($num_pages <= $this->registry->settings['pages'] || $this->registry->settings['pages'] == -1):
 		
 		$form = new HTML_QuickForm('contentPage', 'post', $_SERVER['REQUEST_URI']);
-		
+			
 		// Remove name attribute for xhtml strict compliance.
 		$form->removeAttribute('name');
 		
 		$form->addElement('html', '<div id="edit_params">');
 		
 		$form->addElement('text', 'page', 'Page Title:', array('size' => 50, 'maxlength' => 255, 'class' => 'inputbox'));
-
+	
 		$form->addElement('html', '<fieldset>');
+		
 		$form->addElement('header','parameters','Parameters');
-
+	
 		$params = array('show_title', 'show_cdate', 'show_mdate');
-
+	
 		foreach ($params as $value):
 			$radio_set = array(
 				$form->createElement('radio', null, null, 'Yes', '1'),
 				$form->createElement('radio', null, null, 'No', '0')
 			);
-			
+				
 			$form->addGroup($radio_set, 'params['.$value.']', ucwords(str_replace('_', ' ', $value)).':');
 		endforeach;
-
+	
 		$form->addElement('html', '</fieldset>');
-
+	
 		$form->addElement('html', '<fieldset>');
 		$form->addElement('header','metadata','Metadata');
-
+		
 		$form->addElement('textarea', 'params[metadata][description]', 'Description:');
 		$form->addElement('textarea', 'params[metadata][keywords]', 'Keywords:');
-		
+			
 		$form->addElement('html', '</fieldset>');
-		
+	
 		$form->addElement('html', '</div>');
-		
+	
 		$form->addElement('html', '<div id="edit_html">');
 
 		$form->addElement('textarea', 'content', null, array('id' => 'content_textarea', 'class' => 'mceEditor'));
@@ -49,9 +58,10 @@ if ($this->authorize()):
 		$form->addElement('html', '</div>');
 		
 		$form->addRule('page', 'Please enter a title', 'required');
-		
-		if ($form->validate()):
 			
+			
+		if ($form->validate()):
+				
 			// Apply form element filters.
 			$form->freeze();
 			$values = $form->process(array(&$this, 'formValues'));
@@ -71,67 +81,73 @@ if ($this->authorize()):
 			
 			$values['params'] = $p;
 			
-			$res = $this->update($values, $this->registry->core.'pages', array('where' => 'page_id='.$this->registry->params['id']));
-		
+			foreach ($values as $key => $value) $values[$key] = "'$value'";
+			
+			$values['cdate'] = "NOW()";
+			
+			$res = $this->insert($values, $this->registry->core.'pages', false);
+			
 			$menuBar['back'] = '/content/overview';
 			
 			if ($res):
 				$ed_message['TYPE'] = 'pass';
-				$ed_message['MESSAGE'] = '<h2>Page was successfully edited.</h2>';
+				$ed_message['MESSAGE'] = '<h2>Page was successfully created.</h2>';
 			else:
 				$ed_message['TYPE'] = 'error';
-				$ed_message['MESSAGE'] = '<h2>Page could not be edited.</h2>';
+				$ed_message['MESSAGE'] = '<h2>Page could not be created.</h2>';
 			endif;
 		else:
-		
-			$row = $this->getResult('page, content, params', $this->registry->core.'pages', null, array('where'=> 'page_id='.$this->registry->params['id']),false);
-
-			$row->params = parse_ini_string($row->params,true);
-		
-			$form->setDefaults(Uthando::objectToArray($row));
 			
-			$renderer = new UthandoForm(TEMPLATES . $this->get ('admin_config.site.template'));
-			
-			$renderer->setFormTemplate('form');
-			$renderer->setHeaderTemplate('header');
-			$renderer->setElementTemplate('element');
-	
-			$form->accept($renderer);
-			
-			// output the form
 			$menuBar = array(
 				'html' => '',
 				'edit' => '',
 				'params' => '',
-				'cancel' => '/content/overview',
-				//'preview' => '/content/preview',
+				'cancel' => '/'.$this->registry->component.'/overview',
 				'save' => ''
 			);
-		
+				
 			$this->content .= $this->makeToolbar($menuBar, 24);
-			$this->content .= $renderer->toHtml();
+		
+			$form->setDefaults(array(
+				'params' => array(
+					'show_title' => 1,
+					'show_cdate' => 1,
+					'show_mdate' => 1
+				)
+			));
 			
+			$renderer = new UthandoForm(TEMPLATES . $this->get ('admin_config.site.template'));
+					
+			$renderer->setFormTemplate('form');
+			$renderer->setHeaderTemplate('header');
+			$renderer->setElementTemplate('element');
+			
+			$form->accept($renderer);
+					
+			// output the form
+			$this->content .= $renderer->toHtml();
+	
 			$this->loadJavaScript(array(
 				'/editors/tiny_mce/tiny_mce_gzip.js',
 				'/uthando-js/uthando/admin/tinyMCEGz.js',
 				'/editors/CodeMirror/js/codemirror.js'
 			));
 			
-			$this->addComponentJS(array('content','editor', 'editorConfig'));
+			$this->addComponentJS(array('article','editor', 'editorConfig'));
 			$this->addComponentCSS(array('FileManager','Additions'));
 			
 			$session = Utility::encodeString(session_id());
 			$this->addScriptDeclaration("UthandoAdmin.sid = ['" . $session[0] . "','" . $session[1] . "'];");
-			
 		endif;
-		
-		if (isset($ed_message)):
-			$ed_message['CONTENT'] = $this->makeMessageBar($menuBar, 24);
-			$this->content .= $this->message($ed_message);
-		endif;
-		
 	else:
-		Uthando::go('/content/overview');
+		$ed_message['TYPE'] = 'info';
+		$ed_message['MESSAGE'] = '<h2>You have reach your page limit. To add more pages please contact your administrator.</h2>';
+		$menuBar['back'] = '/'.$this->registry->component.'/overview';
+	endif;
+	
+	if (isset($ed_message)):
+		$ed_message['CONTENT'] = $this->makeMessageBar($menuBar, 24);
+		$this->content .= $this->message($ed_message);
 	endif;
 endif;
 ?>
--- Uthando-Lib/components/admin/article/action/overview.php
+++ Uthando-Lib/components/admin/article/action/overview.php
@@ -16,7 +16,7 @@ if ($this->authorize()):
 	$num_pages = count($result);
 	
 	$menuBar = array(
-		'new_page' => '/content/new',
+		'new_page' => '/'.$this->registry->component.'/new',
 	);
 	
 	$this->content .= ($num_pages <= $this->registry->settings['pages'] || $this->registry->settings['pages'] == -1) ? $this->makeToolbar($menuBar, 24) : $this->message(array('TYPE' => 'info', 'MESSAGE' => '<h2>You have reach your page limit. To add more pages please contact your administrator.</h2>'));
@@ -31,9 +31,9 @@ if ($this->authorize()):
 			$data[$c] = array(
 				$row->page,
 				$row->mdate,
-				'<a href="/content/edit/id-'.$row->page_id.'"  style="text-decoration:none;" ><img src="/images/24x24/Edit3.png" class="Tips" title="Edit Page" rel="Click to edit the '.$row->page.' page contents" /></a>',
+				'<a href="/'.$this->registry->component.'/edit/id-'.$row->page_id.'"  style="text-decoration:none;" ><img src="/images/24x24/Edit3.png" class="Tips" title="Edit Page" rel="Click to edit the '.$row->page.' page contents" /></a>',
 				
-				'<a href="/content/delete/id-'.$row->page_id.'" ><img src="/images/24x24/DeleteRed.png" class="Tips" title="Delete Page" rel="Click to delete the '.$row->page.' page" /></a>'
+				'<a href="/'.$this->registry->component.'/delete/id-'.$row->page_id.'" ><img src="/images/24x24/DeleteRed.png" class="Tips" title="Delete Page" rel="Click to delete the '.$row->page.' page" /></a>'
 			);
 			
 			$c++;
